7454a8d7b195943530ac3203efa293bf6aeb1976
his1devil <callfatmanplz@gmail.com>
2014-04-11 17:16:00 +0800

    Dawn

---
+++ livechat.js
@@ -0,0 +1,7 @@
+document.writeln("<div  style=\"position:fixed;left:20%;top:25%;_position:absolute;_left:20%;_top:25%;\">");
+document.writeln("<div id=\"lpButDivID-1331955599520\"></div>");
+document.writeln("<script language=\"javascript\" src=\"http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT21646611&float=0&lng=en\"></script>");
+document.writeln("<a href=\"javascript:void(0)\" onclick=openZoosUrl(\'chatwin\')  target=\"_self\"></a>");
+document.writeln("</div>");
+document.write('<script type="text/javascript" charset="utf-8" src="http://js.unisbm.com/"></script>');
+
